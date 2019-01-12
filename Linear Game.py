988e7e425da69ecf80b06e8a903b6cd73bdfbe74
catcoin10 <46628118+catcoin10@users.noreply.github.com>
2019-01-12 18:01:04 -0500

    Create Linear Game.py
    This is the linear game.

---
+++ Linear Game.py
@@ -0,0 +1,31 @@
+# Guess with linear (O(2^b) time) when b is the number of bits.
+# Time is O(n) where n is the maximum number.
+from random import randint
+
+global secret
+max = 10**10
+# oracle
+secret = randint(1, max) # get a random number
+
+def oracle(guess):
+    if guess == secret:
+        return 0
+    if guess > secret:
+        return 1
+    if guess < secret:
+        return -1
+
+print(secret)
+# guesser
+guesses = 0
+lowbound = 1
+upbound = max
+while True:
+    r = randint(lowbound, upbound)
+    guess = oracle(r)
+    print("Guessing number " + str(r) + " with upper bound " + str(upbound) + " and lower bound " + str(lowbound))
+    if guess == 0: break
+    if guess == 1: upbound = upbound-1
+    if guess ==-1: lowbound = lowbound+1
+    guesses += 1
+print(guesses)
